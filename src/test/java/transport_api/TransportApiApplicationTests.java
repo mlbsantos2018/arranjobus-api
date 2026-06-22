6167c68de5f41a9321baf7c45bdbb3ad7bd1aad9
mlbsantos2018 <mlbsantos2018>
2026-06-22 15:50:41 -0300

    Initial commit

---
+++ src/test/java/transport_api/TransportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package transport_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
